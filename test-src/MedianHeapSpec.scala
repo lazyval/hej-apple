fac8bef04302b3452642a2ccd2464d59b596eb5a
Kostya Golikov <johnysilver7@gmail.com>
2016-01-22 13:08:54 +0300

    Reformatted code

--- test-src/MedianHeapSpec.scala
+++ test-src/MedianHeapSpec.scala
@@ -9,22 +9,22 @@ class ExampleSpec extends FlatSpec with Matchers {
     heap.push(2)
     heap.push(3)
     heap.push(4)
-    heap.pop() should be (3)
-    heap.pop() should be (2)
-    heap.pop() should be (4)
-    heap.pop() should be (1)
+    heap.pop() should be(3)
+    heap.pop() should be(2)
+    heap.pop() should be(4)
+    heap.pop() should be(1)
   }
 
   it should "return elements in push order if pop in called after every addition" in {
     val heap = new MedianHeap
     heap.push(1)
-    heap.pop() should be (1)
+    heap.pop() should be(1)
     heap.push(2)
-    heap.pop() should be (2)
+    heap.pop() should be(2)
     heap.push(3)
-    heap.pop() should be (3)
+    heap.pop() should be(3)
     heap.push(4)
-    heap.pop() should be (4)
+    heap.pop() should be(4)
   }
 
   it should "work correctly when duplicates presented" in {
@@ -32,16 +32,16 @@ class ExampleSpec extends FlatSpec with Matchers {
     heap.push(0)
     heap.push(0)
     heap.push(0)
-    heap.pop() should be (0)
-    heap.pop() should be (0)
-    heap.pop() should be (0)
+    heap.pop() should be(0)
+    heap.pop() should be(0)
+    heap.pop() should be(0)
     heap.push(4)
-    heap.pop() should be (4)
+    heap.pop() should be(4)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyHeap = new MedianHeap
-    a [NoSuchElementException] should be thrownBy {
+    a[NoSuchElementException] should be thrownBy {
       emptyHeap.pop()
     }
   }
