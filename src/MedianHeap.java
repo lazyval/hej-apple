42f86c70c067a524308fb2d1320a63584e3241cd
Kostya Golikov <johnysilver7@gmail.com>
2016-01-24 21:37:23 +0300

    Reformatting java code

--- src/MedianHeap.java
+++ src/MedianHeap.java
@@ -4,69 +4,69 @@ import java.util.NoSuchElementException;
 import java.util.Collections;
 
 public class MedianHeap {
-  private final static int DEFAULT_CAPACITY = 11;
-  private PriorityQueue<Integer> leftHalf = mkMaxHeap();
-  private PriorityQueue<Integer> rightHalf = mkMinHeap();
+    private final static int DEFAULT_CAPACITY = 11;
+    private PriorityQueue<Integer> leftHalf = mkMaxHeap();
+    private PriorityQueue<Integer> rightHalf = mkMinHeap();
 
-  public int pop() {
-    if (isEmpty()) throw new NoSuchElementException();
+    public int pop() {
+        if (isEmpty()) throw new NoSuchElementException();
 
-    int result = size() % 2 == 0
-      ? rightHalf.poll()
-      : leftHalf.poll();
+        int median = size() % 2 == 0
+            ? rightHalf.poll()
+            : leftHalf.poll();
 
-    return result;
-  }
-
-  public void push(int x) {
-    if (isEmpty()) {
-      leftHalf.add(x);
-    } else {
-      if (leftHalf.peek() >= x) {
-        leftHalf.add(x);
-      } else {
-        rightHalf.add(x);
-      }
+        return median;
     }
 
-    rebalance();
-  }
+    public void push(int x) {
+        if (isEmpty()) {
+            leftHalf.add(x);
+        } else {
+            if (leftHalf.peek() >= x) {
+                leftHalf.add(x);
+            } else {
+                rightHalf.add(x);
+            }
+        }
 
-  private boolean isEmpty() {
-    return size() == 0;
-  }
-
-  private int size() {
-    return leftHalf.size() + rightHalf.size();
-  }
-
-  private void rebalance() {
-    boolean isRequired = Math.abs(leftHalf.size() - rightHalf.size()) > 1;
-    if (isRequired) {
-      if (leftHalf.size() > rightHalf.size()) {
-        rightHalf.add(leftHalf.poll());
-      } else {
-        leftHalf.add(rightHalf.poll());
-      }
+        rebalance();
     }
-  }
 
-  private static PriorityQueue<Integer> mkMinHeap() {
-    return new PriorityQueue<Integer>(DEFAULT_CAPACITY, NATURAL_ORDER);
-  }
-
-  private static PriorityQueue<Integer> mkMaxHeap() {
-    return new PriorityQueue<Integer>(DEFAULT_CAPACITY, Collections.reverseOrder(NATURAL_ORDER));
-  }
-
-  private final static Comparator<Integer> NATURAL_ORDER = new Comparator<Integer>() {
-    @Override
-    public int compare(Integer a, Integer b) {
-      return a.compareTo(b);
+    private boolean isEmpty() {
+        return size() == 0;
     }
-  };
 
-  public String toString() {
-    return leftHalf.toString() + rightHalf.toString();
-  }
+    private int size() {
+        return leftHalf.size() + rightHalf.size();
+    }
+
+    private void rebalance() {
+        boolean isRequired = Math.abs(leftHalf.size() - rightHalf.size()) > 1;
+        if (isRequired) {
+            if (leftHalf.size() > rightHalf.size()) {
+                rightHalf.add(leftHalf.poll());
+            } else {
+                leftHalf.add(rightHalf.poll());
+            }
+        }
+    }
+
+    private static PriorityQueue<Integer> mkMinHeap() {
+        return new PriorityQueue<Integer>(DEFAULT_CAPACITY, NATURAL_ORDER);
+    }
+
+    private static PriorityQueue<Integer> mkMaxHeap() {
+        return new PriorityQueue<Integer>(DEFAULT_CAPACITY, Collections.reverseOrder(NATURAL_ORDER));
+    }
+
+    private final static Comparator<Integer> NATURAL_ORDER = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return a.compareTo(b);
+        }
+    };
+
+    public String toString() {
+        return leftHalf.toString() + rightHalf.toString();
+    }
 }
