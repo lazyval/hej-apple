9e4bfaab43c20b322ff8e063d59b291ec4bf8025
Kostya Golikov <johnysilver7@gmail.com>
2016-01-25 00:04:06 +0300

    Added solution for a phone book task

---
+++ src/PhonePrefixes.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class PhonePrefixes {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Solver {
+        public boolean solve(String[] lines) {
+            Arrays.sort(lines);
+            for (int i = 0; i < lines.length - 1; i++) {
+                String possiblePrefix = lines[0];
+                String word = lines[1];
+                if (word.startsWith(possiblePrefix)) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ test-src/PhonePrefixesSpec.scala
@@ -0,0 +1,46 @@
+import PhonePrefixes.Solver
+import org.scalatest.concurrent.Timeouts
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.util.Random
+
+class PhonePrefixesSpec extends FlatSpec with Matchers with Timeouts {
+  "Solver" should "return true for solvable problem" in {
+    val solvable = Array(
+      "113",
+      "12340",
+      "123440",
+      "12345",
+      "98346"
+    )
+
+    (new Solver).solve(solvable) should be(true)
+  }
+
+  it should "return false for non-solvable example" in {
+    val nonSolvable = Array(
+      "911",
+      "97625999",
+      "91125426"
+    )
+
+    (new Solver).solve(nonSolvable) should be(false)
+  }
+
+  it should "0911 is a different phone number from 911" in {
+    val solvable = Array("911", "0911")
+
+
+    (new Solver).solve(solvable) should be(true)
+  }
+
+  it should "execute fast for very large sequences" in {
+    import org.scalatest.time.SpanSugar._
+
+    val bigInput = Random.alphanumeric.grouped(10).map(x => x.mkString).take(100000).toArray
+
+    failAfter(100 millis) {
+      (new Solver).solve(bigInput)
+    }
+  }
+}
